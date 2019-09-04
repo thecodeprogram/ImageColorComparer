1dba40fea8ab2322d44d3eb0607f9b7010416f71
thecodeprogram <43945916+thecodeprogram@users.noreply.github.com>
2019-09-04 20:21:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace RenkKarsilastir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.f1 = new System.Windows.Forms.OpenFileDialog();
+            this.f2 = new System.Windows.Forms.OpenFileDialog();
+            this.r2 = new System.Windows.Forms.PictureBox();
+            this.r1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.k = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.r2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(636, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START COMPARING";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // f1
+            // 
+            this.f1.FileName = "f1";
+            // 
+            // f2
+            // 
+            this.f2.FileName = "openFileDialog2";
+            // 
+            // r2
+            // 
+            this.r2.Image = global::RenkKarsilastir.Properties.Resources.ResimYok;
+            this.r2.Location = new System.Drawing.Point(393, 12);
+            this.r2.Name = "r2";
+            this.r2.Size = new System.Drawing.Size(375, 340);
+            this.r2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.r2.TabIndex = 6;
+            this.r2.TabStop = false;
+            this.r2.Click += new System.EventHandler(this.r2_Click);
+            // 
+            // r1
+            // 
+            this.r1.Image = global::RenkKarsilastir.Properties.Resources.ResimYok;
+            this.r1.Location = new System.Drawing.Point(12, 12);
+            this.r1.Name = "r1";
+            this.r1.Size = new System.Drawing.Size(375, 340);
+            this.r1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.r1.TabIndex = 0;
+            this.r1.TabStop = false;
+            this.r1.Click += new System.EventHandler(this.r1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 358);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(618, 23);
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.progressBar1_MouseDown);
+            // 
+            // k
+            // 
+            this.k.Description = "ÇIKTI ALINACAK KLASORU SEÇİNİZ";
+            this.k.SelectedPath = "C:\\Users\\3NB\\Desktop";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "CURRENT PROCESS : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 423);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.r2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.r1);
+            this.Name = "Form1";
+            this.Text = "RESİM KARŞILAŞTIRICI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.r2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox r1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox r2;
+        private System.Windows.Forms.OpenFileDialog f1;
+        private System.Windows.Forms.OpenFileDialog f2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog k;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace RenkKarsilastir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "Please select an image";
+        }
+
+        string filePath;
+        int firstSize = 0;
+        int secondSize = 0;
+
+        public void resimkarsilastir()
+        {
+            StreamReader first_read = File.OpenText(filePath + "\\first_image.txt");
+            StreamReader second_read = File.OpenText(filePath + "\\second_image.txt");
+
+            StreamWriter same_write = File.AppendText(filePath + "\\same_codes.txt");
+            StreamWriter firstDiff = File.AppendText(filePath + "\\differences_of_first.txt");
+            StreamWriter secondDiff = File.AppendText(filePath + "\\differences_os_seconds.txt");
+
+            string m1;
+            string m2;
+
+            try
+            {
+                m1 = first_read.ReadLine();
+                m2 = second_read.ReadLine();
+
+                int same_codes = 0;
+                int first_codes = 0;
+                int second_codes = 0;
+
+                for (int i = 0; i < Math.Max(firstSize, secondSize); i++)
+                {
+                    if (m1 == m2)
+                    {
+                        same_write.WriteLine(m1);
+                        same_codes++;
+                    }
+                    else if (m1 != m2)
+                    {
+                        firstDiff.WriteLine(m1);
+                        first_codes++;
+                        secondDiff.WriteLine(m2);
+                        second_codes++;
+                    }
+                    m1 = first_read.ReadLine();
+                    m2 = second_read.ReadLine();
+
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    same_write.WriteLine();
+                    firstDiff.WriteLine();
+                    secondDiff.WriteLine();
+                }
+                same_write.WriteLine();
+                firstDiff.WriteLine();
+                secondDiff.WriteLine();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    same_write.WriteLine();
+                    firstDiff.WriteLine();
+                    secondDiff.WriteLine();
+                }
+
+                firstDiff.WriteLine();
+
+                StreamWriter reporting = File.AppendText(filePath + "\\sonuc.txt");
+                reporting.WriteLine("DOSYA KARŞILAŞTIRMA RAPORU");
+                reporting.WriteLine();
+                reporting.WriteLine((same_codes - 100).ToString() + " SAME CODE..");
+                reporting.WriteLine();
+                reporting.WriteLine("IN FIRST FILE " + (first_codes - 100).ToString() + " DIFFERENT CODE...");
+                reporting.WriteLine();
+                reporting.WriteLine("IN SECOND FILE " + (second_codes - 100).ToString() + " DIFFERENT CODE...");
+                reporting.WriteLine();
+                reporting.WriteLine("IN FIRST FILE " + (firstSize).ToString() + " COLOR CODES...");
+                reporting.WriteLine();
+                reporting.WriteLine("IN SECOND FILE " + (secondSize).ToString() + " COLOR CODES...");
+                reporting.WriteLine();
+                reporting.WriteLine("AS A RESULT FROM THESE VALUES THE DIFFERENCES : %" + Convert.ToString((100 * first_codes) / firstSize));
+                reporting.Close();
+
+
+                same_write.Close();
+                first_read.Close();
+                second_read.Close();
+
+            }
+            catch (Exception errr) { MessageBox.Show("An error occuered :  " + errr.ToString()); }
+            finally { }
+
+        }
+        public void get_image2()
+        {
+            if (r1.Image != null && r2.Image != null)
+            {
+
+                Bitmap my_image = new System.Drawing.Bitmap(r2.Image);
+                Color my_color;
+                int i, j;
+                secondSize = my_image.Height * my_image.Width + 100;
+                progressBar1.Maximum = my_image.Width * my_image.Height;
+                StreamWriter wrtr = File.AppendText(filePath + "\\second_image.txt");
+                for (i = 0; i <= my_image.Width - 1; i++)
+                {
+                    for (j = 0; j <= my_image.Height - 1; j++)
+                    {
+                        my_color = my_image.GetPixel(i, j);
+
+                        wrtr.WriteLine("(" + j.ToString() + "," + i.ToString() + ")" + " - " + my_color.ToString());
+
+                        if ((i % 1) == 0)
+                        {
+                            progressBar1.Value = i * my_image.Height + j;
+                        }
+
+                    }
+                }
+                wrtr.Close();
+                r1.Image = my_image;
+            }
+            else
+            {
+                MessageBox.Show("Select An Image");
+            }
+        }
+
+        public void get_image1()
+        {
+            
+            if (r1.Image != null && r2.Image!= null)
+            {
+                Bitmap my_image = new System.Drawing.Bitmap(r1.Image);
+                Color my_color;
+                int i, j;
+                firstSize = my_image.Height * my_image.Width;
+                progressBar1.Maximum = my_image.Width * my_image.Height + 100;
+                StreamWriter wrtr= File.AppendText(filePath + "\\first_image.txt");
+                for (i = 0; i <= my_image.Width - 1; i++)
+                {
+                    for (j = 0; j <= my_image.Height - 1; j++)
+                    {
+                        my_color = my_image.GetPixel(i, j);
+
+                        wrtr.WriteLine("(" + j.ToString() + "," + i.ToString() + ")" + " - " + my_color.ToString());
+
+                        if ((i % 1) == 0)
+                        {
+                            progressBar1.Value = i * my_image.Height + j;
+                        }
+
+                    }
+                }
+                wrtr.Close();
+                r1.Image = my_image;
+            }
+            else
+            {
+               MessageBox.Show("Select An Image");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (k.ShowDialog() == DialogResult.OK)
+            {
+                filePath = k.SelectedPath.ToString();
+                label2.Text = "First image encoding...";
+                get_image1();
+                label2.Text = "Second image encoding...";
+                get_image2();
+                label2.Text = "Comparing...";
+                resimkarsilastir();
+                MessageBox.Show("Completed", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void r1_Click(object sender, EventArgs e)
+        {
+            f1.FileName = "";
+            f1.Title = "Please select an image";
+            f1.Filter = "(*.jpg)|*.jpg|(*.jpeg)|*.jpeg|(*.png)|*.png|(*.bmp)|*.bmp";
+            f1.Multiselect = false;
+            if (f1.ShowDialog() == DialogResult.OK)
+            {
+                r1.ImageLocation = f1.FileName.ToString();
+            }
+        }
+
+        private void r2_Click(object sender, EventArgs e)
+        {
+            f2.FileName = "";
+            f2.Title = "Please select an image";
+            f1.Filter = "(*.jpg)|*.jpg|(*.jpeg)|*.jpeg|(*.png)|*.png|(*.bmp)|*.bmp";
+            f2.Multiselect = false;
+            if (f2.ShowDialog() == DialogResult.OK)
+            {
+                r2.ImageLocation = f2.FileName.ToString();
+            }
+        }
+
+        private void progressBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
